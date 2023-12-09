bca1eab12797a4a5c38d8124ca5eea1eef88d187
h1Vincent/h1 <vincentgasatan21@gmail.com>
2023-12-09 20:17:22 +0800

    feat: add colour scheme for site

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      'about.html',
+      'index.html',
+      'mywork.html',
+      'contact.html',
+      'projects.html',
+      'resume.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
